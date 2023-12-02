434f3bb30e356e09914e3018e36e97e1f4f2a376
Christof <contact@peatral.xyz>
2023-12-03 00:34:25 +0100

    ✨ Make runner output fancier

--- src/run_all.js
+++ src/run_all.js
@@ -1,18 +1,32 @@
 const fs = require("fs");
-const { readLines } = require("./utility");
+const { readLines, range } = require("./utility");
 
+const days = [];
 for (let day = 1; day <= 25; day++) {
   const path = "day_" + ("0" + day).slice(-2);
   if (fs.existsSync("src/" + path)) {
-    console.log(`Day ${day}`);
+    console.log(`\x1b[1m\x1b[37m> Running day ${day} <\x1b[0m\n`);
 
     const { partA, partB } = require(`./${path}/task`);
 
-    console.log("Part A");
-    console.log(partA(readLines(`src/${path}/input.txt`)));
-    console.log("\nPart B");
-    console.log(partB(readLines(`src/${path}/input.txt`)));
+    console.log(
+      `\x1b[1m\x1b[30m\x1b[42m Part A \x1b[0m\x1b[1m\x1b[37m ${partA(
+        readLines(`src/${path}/input.txt`),
+      )}\x1b[0m`,
+    );
+    console.log(
+      `\x1b[1m\x1b[30m\x1b[42m Part B \x1b[0m\x1b[1m\x1b[37m ${partB(
+        readLines(`src/${path}/input.txt`),
+      )}\x1b[0m`,
+    );
 
     console.log("\n");
+
+    days.push(day);
   }
 }
+
+console.log(`\x1b[1m\x1b[37mTotal Days executed:    \x1b[0m ${days.length}`);
+console.log(
+  `\x1b[1m\x1b[37mTotal Days not executed:\x1b[0m ${25 - days.length}`,
+);
