60e8af4f9ff0f49ffdee5c01275348ba1a40cbbc
Christof <contact@peatral.xyz>
2023-12-03 01:01:58 +0100

    🥅 Handle errors in runner

--- src/run_all.js
+++ src/run_all.js
@@ -1,40 +1,64 @@
 const fs = require("fs");
 const { readLines } = require("./utility");
 
-const executed_days = [];
-const errored_days = [];
+const completedDays = [];
+const erroredDays = [];
 
 for (let day = 1; day <= 25; day++) {
   const path = "day_" + ("0" + day).slice(-2);
   if (fs.existsSync("src/" + path)) {
     console.log(`\x1b[1m\x1b[37m> Running day ${day} <\x1b[0m\n`);
+    let lines = [];
+    let partA, partB;
     try {
-      const { partA, partB } = require(`./${path}/task`);
-
-      console.log(
-        `\x1b[1m\x1b[30m\x1b[42m Part A \x1b[0m\x1b[1m\x1b[37m ${partA(
-          readLines(`src/${path}/input.txt`),
-        )}\x1b[0m`,
-      );
-      console.log(
-        `\x1b[1m\x1b[30m\x1b[42m Part B \x1b[0m\x1b[1m\x1b[37m ${partB(
-          readLines(`src/${path}/input.txt`),
-        )}\x1b[0m`,
-      );
-
-      executed_days.push(day);
+      lines = readLines(`src/${path}/input.txt`);
+      ({ partA, partB } = require(`./${path}/task`));
     } catch (e) {
       console.log(`\x1b[1m\x1b[31mError while executing...\x1b[0m`);
-      errored_days.push(day);
+      erroredDays.push(day);
+      continue;
+    }
+
+    let error = false;
+    try {
+      console.log(
+        `\x1b[1m\x1b[30m\x1b[42m Part A \x1b[0m\x1b[1m\x1b[37m ${partA(
+          lines,
+        )}\x1b[0m`,
+      );
+    } catch (e) {
+      console.log(
+        `\x1b[1m\x1b[37m\x1b[41m Part A \x1b[0m\x1b[1m\x1b[37m ${e}\x1b[0m`,
+      );
+      error = true;
+    }
+
+    try {
+      console.log(
+        `\x1b[1m\x1b[30m\x1b[42m Part B \x1b[0m\x1b[1m\x1b[37m ${partB(
+          lines,
+        )}\x1b[0m`,
+      );
+    } catch (e) {
+      console.log(
+        `\x1b[1m\x1b[37m\x1b[41m Part B \x1b[0m\x1b[1m\x1b[37m ${e}\x1b[0m`,
+      );
+      error = true;
+    }
+
+    if (error) {
+      erroredDays.push(day);
+    } else {
+      completedDays.push(day);
     }
     console.log("\n");
   }
 }
 
-console.log(`\x1b[1m\x1b[37mExecuted Days:\x1b[0m ${executed_days.length}`);
-console.log(`\x1b[1m\x1b[37mErrored Days: \x1b[0m ${errored_days.length}`);
+console.log(`\x1b[1m\x1b[37mCompleted Days:\x1b[0m ${completedDays.length}`);
+console.log(`\x1b[1m\x1b[37mErrored Days:  \x1b[0m ${erroredDays.length}`);
 console.log(
-  `\x1b[1m\x1b[37mSkipped Days: \x1b[0m ${
-    25 - errored_days.length - executed_days.length
+  `\x1b[1m\x1b[37mSkipped Days:  \x1b[0m ${
+    25 - erroredDays.length - completedDays.length
   }\n\n`,
 );
